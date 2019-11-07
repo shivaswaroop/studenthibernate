597c9c8a96d6fe2cfc10bb2a92a1123d92ec1b1d
shivaswaroop <shivaswaroops@gmail.com>
2019-11-06 18:20:17 -0800

    new student hibernate

---
+++ student_ex/src/main/java/com/lti/hibernate_ex/student_ex/App.java
@@ -0,0 +1,25 @@
+package com.lti.hibernate_ex.student_ex;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration conf = new Configuration().configure();
+        StandardServiceRegistryBuilder build= new StandardServiceRegistryBuilder().applySettings(conf.getProperties());
+        SessionFactory fact = conf.buildSessionFactory(build.build());
+        Session session=fact.openSession();
+        Student stu=new Student();
+        stu.setRoll_num(1);
+        stu.setName("anu");
+        stu.setDept("cs");
+        session.beginTransaction();
+        session.save(stu);
+        session.getTransaction().commit();
+    }
+}
---
+++ student_ex/src/main/java/com/lti/hibernate_ex/student_ex/Student.java
@@ -0,0 +1,26 @@
+package com.lti.hibernate_ex.student_ex;
+
+public class Student {
+private String name;
+private int roll_num;
+private String dept;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getRoll_num() {
+	return roll_num;
+}
+public void setRoll_num(int roll_num) {
+	this.roll_num = roll_num;
+}
+public String getDept() {
+	return dept;
+}
+public void setDept(String dept) {
+	this.dept = dept;
+}
+
+}
